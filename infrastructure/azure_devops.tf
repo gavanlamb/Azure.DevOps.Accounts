d7123feed866784f04662a51e73b6b9ec3af5809
Gavan Lamb <gavan.jon.lamb@gmail.com>
2022-09-06 23:02:08 +1000

    Remove environments

--- infrastructure/azure_devops.tf
+++ infrastructure/azure_devops.tf
@@ -19,13 +19,3 @@ resource "azuredevops_project" "project" {
     "artifacts" = "enabled"
   }
 }
-resource "azuredevops_environment" "production" {
-  for_each = {for adp in var.azure_devops_projects:  adp.name => adp}
-  project_id = azuredevops_project.project[each.key].id
-  name = "Production"
-}
-resource "azuredevops_environment" "preview" {
-  for_each = {for adp in var.azure_devops_projects:  adp.name => adp}
-  project_id = azuredevops_project.project[each.key].id
-  name = "Preview"
-}
